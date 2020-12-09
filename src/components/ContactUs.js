0c303ae4dbbc30b862238954517d5e69466f34e5
IslamHamad <islam.elnil95@gmail.com>
2020-12-09 17:49:11 +0200

    Adding a post comment feature

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,45 +1,76 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
+import { v4 as uuidV4 } from "uuid";
 
-const ContactUs=()=> {
-    const [contact, setContact]=useState({
-        email:"",
-        subject:"",
-        message:""
-    })
+const ContactUs = () => {
+  const [commentsArray, setCommentsArray] = useState([{
+      id:"23232",
+      name:"Islam",
+      comment:"first Comment"
+  }]);
+  const [newComment, setNewComment] = useState({
+    id: "",
+    name: "",
+    comment: "",
+  });
 
-    const updateContactForm=(e)=>{
-        setContact({
-            ...contact,
-            [e.target.name]:e.target.value
-        })
-    }
+  const addNewComment = (e) => {
+    setNewComment({
+      ...newComment,
+      id: uuidV4(),
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const submitMessage=(e)=>{
-        e.preventDefault()
-        setContact({
-            ...contact,
-            email:"",
-            subject:"",
-            message:""
-        })
-    }
+  const submitNewComment = (e) => {
+    e.preventDefault();
+    setCommentsArray([...commentsArray, { ...newComment }]);
+    setNewComment({
+      ...newComment,
+      id: "",
+      name: "",
+      comment: "",
+    });
+  };
 
-    return (
-        <div className="contact-us-section">
-        <form className="contact-us-form" onSubmit={submitMessage}>
-        <h1 id="contact-us-title">COMMENT ON THIS POST</h1>
-        <label id="email-label">NAME:</label><br/>
-        <input id="email-input"  name="name" required onChange={updateContactForm}/><br/>
-        <label id="email-label">EMAIL:</label><br/>
-        <input id="email-input" type="email" placeholder="ex:jondoe@gmail.com" name="email" required onChange={updateContactForm}/><br/>
-      
-        <label id="message-label">COMMENT:</label><br/>
-        <textarea  id="message-input"  name="message" required onChange={updateContactForm}/><br/>
- 
-        <input id="submit-button"  type="submit" value="Send"/>
-        </form>
+  return (
+    <div className="contact-us-section">
+    <h1>POST A COMMENT</h1>
+    <div className="comments">
+      {commentsArray.map((comment) => (
+        <div key={comment.id} id="comment">
+        <div id="date" style={{float:"right"}}>{new Date().toDateString()}</div>
+          <h2> {comment.name}</h2>
+          <p>{comment.comment}</p>
         </div>
-    )
-}
+      ))}
+      </div>
 
-export default ContactUs
+      <form className="contact-us-form" onSubmit={submitNewComment}>
+        <label id="email-label">NAME:</label>
+        <br />
+        <input
+          id="email-input"
+          value={newComment.name}
+          name="name"
+          required
+          onChange={addNewComment}
+        />
+        <br />
+        <label id="message-label">COMMENT:</label>
+        <br />
+        <textarea
+          id="message-input"
+          name="comment"
+          required
+          value={newComment.comment}
+          onChange={addNewComment}
+        />
+        <br />
+
+        <input id="submit-button" type="submit" value="Send" />
+      </form>
+    </div>
+  );
+};
+
+export default ContactUs;
