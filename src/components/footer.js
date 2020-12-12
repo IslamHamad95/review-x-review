85e3d195a73cdae181e4d3bb49a5675f885a2231
IslamHamad <islam.elnil95@gmail.com>
2020-12-12 14:25:11 +0200

    changing footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer=()=> {
     return (
         <footer>
-        Copyright Islam Hussein 2020. All Rights Reserved
+         Islam Hussein 2020. 
         </footer>
     )
 }
