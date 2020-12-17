b256095d7c13cda49c91ec8d32a33284ed58671b
IslamHamad <islam.elnil95@gmail.com>
2020-12-17 16:15:55 +0200

    adding changes to the sidebar

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 import logo from "../storage/logo.png";
@@ -7,34 +7,58 @@ import Login from "./Login";
 const Header = ({ userName }) => {
   const [popUp, setPopUp] = useState(false);
   const [sidebar, setSidebar] = useState(false);
+  const wrapperRef = useRef(0);
+
   const togglePopUp = () => {
     setPopUp((popUp) => (popUp = !popUp));
   };
 
   const displaySideBar = () => {
-    setSidebar(sidebar=>(sidebar = !sidebar));
+    setSidebar((sidebar) => (sidebar = !sidebar));
   };
+
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(event) {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        displaySideBar();
+      }
+    }
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [wrapperRef]);
 
   return (
     <div>
-    <span className="hamburger" onClick={displaySideBar}>
-    <div className="line"></div>
-    <div className="line"></div>
-    <div className="line"></div>
-  </span>
-      
-        {sidebar ? (
-          <nav className="navbar">
-        
+      {!sidebar?(<span className="hamburger" onClick={displaySideBar}>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </span>):null}
+
+      {sidebar ? (
+        <nav ref={wrapperRef} className="navbar">
           <ul className="nav-tabs">
-          <li>
-          <NavLink  to="/" onClick={displaySideBar}>HOME</NavLink>
-          </li>
+            <li>
+              <NavLink to="/" onClick={displaySideBar}>
+                HOME
+              </NavLink>
+            </li>
             <li id="news-nav">
-              <NavLink to="/news" onClick={displaySideBar}>NEWS</NavLink>
+              <NavLink to="/news" onClick={displaySideBar}>
+                NEWS
+              </NavLink>
             </li>
             <li id="reviews-nav">
-              <NavLink to="/reviews" onClick={displaySideBar}>REVIEWS</NavLink>
+              <NavLink to="/reviews" onClick={displaySideBar}>
+                REVIEWS
+              </NavLink>
             </li>
             <li id="podcast-nav">
               <a href="https://anchor.fm/checkpod">PODCAST</a>
@@ -47,24 +71,26 @@ const Header = ({ userName }) => {
                 </a>
               </div>
             ) : (
-          
-              <button style={{display:"none"}} id="login-nav" onClick={togglePopUp}>
+              <button
+                style={{ display: "none" }}
+                id="login-nav"
+                onClick={togglePopUp}
+              >
                 SIGN IN
-              </button>)}
+              </button>
+            )}
 
-              {userName ? (
-                <div className="logged-in">
-                  <NavLink to="/addpost" id="login-signup">
-                    ADD POST
-                  </NavLink>
-                </div>
-              ) : null}
-              
-           
+            {userName ? (
+              <div className="logged-in">
+                <NavLink to="/addpost" id="login-signup">
+                  ADD POST
+                </NavLink>
+              </div>
+            ) : null}
           </ul>
-          </nav>
-        ) : null}
-      
+        </nav>
+      ) : null}
+
       <header>
         <NavLink to="/">
           <img id="logo" alt="logo" src={logo}></img>
@@ -102,7 +128,11 @@ const Header = ({ userName }) => {
               </a>
             </div>
           ) : (
-            <button style={{display:"none"}} id="login-signup" onClick={togglePopUp}>
+            <button
+              style={{ display: "none" }}
+              id="login-signup"
+              onClick={togglePopUp}
+            >
               SIGN IN
             </button>
           )}
